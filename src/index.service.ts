9dce0cfd5e210d83f8bae57457915672d8e96e6f
An Nguyen Q <ngquangan.dev@gmail.com>
2022-03-05 23:51:39 +0700

    feat(auth0): add module & integrate auth0

--- src/index.service.ts
+++ src/index.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthenticationClient, ManagementClient } from 'auth0';
+import {
+  AUTH0_CLIENT_TOKEN,
+  AUTH0_MANAGEMENT_TOKEN,
+  AUTH0_OPTIONS,
+} from './constants';
+import { IAuth0Options } from './types';
 
 @Injectable()
 export class Auth0Service {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject(AUTH0_OPTIONS)
+    private readonly auth0Options: IAuth0Options,
+    @Inject(AUTH0_MANAGEMENT_TOKEN)
+    private readonly managementClient: ManagementClient,
+    @Inject(AUTH0_CLIENT_TOKEN)
+    private readonly authClient: AuthenticationClient,
+  ) {}
+
+  async getManagementClient() {
+    return this.managementClient;
+  }
+
+  async getAuthClient() {
+    return this.authClient;
+  }
+
+  async getAuthenticatedToken() {
+    return this.authClient.clientCredentialsGrant({
+      audience: this.auth0Options.audience,
+    });
+  }
+
+  async setAppMetadata(auth0UserId: string, metadata: Record<string, any>) {
+    return this.managementClient.updateAppMetadata(
+      { id: auth0UserId },
+      metadata,
+    );
+  }
+
+  async setUserMetadata(auth0UserId: string, metadata: Record<string, any>) {
+    return this.managementClient.updateUserMetadata(
+      { id: auth0UserId },
+      metadata,
+    );
   }
 }
